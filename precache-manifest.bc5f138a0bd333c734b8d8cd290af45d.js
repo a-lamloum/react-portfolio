09e41462201a5df9bbe894f4648f8d778bbc1093
Ahmed Lamloum <ahmadomar.sci@gmail.com>
2022-06-02 22:42:07 +0200

    Updates

---
+++ precache-manifest.bc5f138a0bd333c734b8d8cd290af45d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "da6154f4d9f214f67af4",
+    "url": "/react-portfolio/static/css/main.9c5e6ce7.chunk.css"
+  },
+  {
+    "revision": "da6154f4d9f214f67af4",
+    "url": "/react-portfolio/static/js/main.4281f6ff.chunk.js"
+  },
+  {
+    "revision": "94872cf7e81768d148a3",
+    "url": "/react-portfolio/static/js/runtime~main.f974a8eb.js"
+  },
+  {
+    "revision": "51eee9c5b4199f3c6a72",
+    "url": "/react-portfolio/static/css/2.431c0440.chunk.css"
+  },
+  {
+    "revision": "51eee9c5b4199f3c6a72",
+    "url": "/react-portfolio/static/js/2.15671c1d.chunk.js"
+  },
+  {
+    "revision": "b3312b574e432b0a10f2d1e9c59a572c",
+    "url": "/react-portfolio/static/media/cv.b3312b57.pdf"
+  },
+  {
+    "revision": "1ced51225c68d43606294087737614cf",
+    "url": "/react-portfolio/static/media/hero-me2.1ced5122.png"
+  },
+  {
+    "revision": "0b1dc761ac8678f232f04467d361e012",
+    "url": "/react-portfolio/static/media/img.0b1dc761.jpeg"
+  },
+  {
+    "revision": "08467698c6f22c7ce16aebe61fbe7b04",
+    "url": "/react-portfolio/static/media/ezlearn.08467698.jpg"
+  },
+  {
+    "revision": "a9f7aaf8e079e3d7c0a6d7ea4f4752b3",
+    "url": "/react-portfolio/static/media/library.a9f7aaf8.jpg"
+  },
+  {
+    "revision": "22783fb816e2293d1ed17ac67d6d6aa6",
+    "url": "/react-portfolio/static/media/tindog.22783fb8.jpg"
+  },
+  {
+    "revision": "a775b70885caadaa3c4b9af9be80e789",
+    "url": "/react-portfolio/static/media/dice.a775b708.jpg"
+  },
+  {
+    "revision": "08f895be0704ea911a11996e2b6f928c",
+    "url": "/react-portfolio/static/media/karamella.08f895be.jpg"
+  },
+  {
+    "revision": "f74941a20bdece66723dcde94f83ce1a",
+    "url": "/react-portfolio/static/media/avatar1.f74941a2.jpg"
+  },
+  {
+    "revision": "bdc0aa10eae70ccc9c97fdae1521d50e",
+    "url": "/react-portfolio/index.html"
+  }
+];
